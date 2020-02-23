949acb7206a230426fb51b07bebd409a85c14118
aleksJoker <aleks.valushko@yandex.ru>
2020-02-23 13:02:21 +0300

    Updates

---
+++ precache-manifest.f2023681e4b51557bd9e7fc0fd8c5f04.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe439d1f110b565b5d6eac1a0ac6263",
+    "url": "/aleksvalushko/portfolio/index.html"
+  },
+  {
+    "revision": "289252d4f91bed2728af",
+    "url": "/aleksvalushko/portfolio/static/css/main.b9eb2d11.chunk.css"
+  },
+  {
+    "revision": "7a7a418ce0f13706cde0",
+    "url": "/aleksvalushko/portfolio/static/js/2.b027938b.chunk.js"
+  },
+  {
+    "revision": "289252d4f91bed2728af",
+    "url": "/aleksvalushko/portfolio/static/js/main.d73f77c8.chunk.js"
+  },
+  {
+    "revision": "09532b659e3d82337e77",
+    "url": "/aleksvalushko/portfolio/static/js/runtime-main.3b894a2e.js"
+  },
+  {
+    "revision": "9f48048d2bb8b1989ae950063dcc163f",
+    "url": "/aleksvalushko/portfolio/static/media/aboutMe.9f48048d.jpg"
+  },
+  {
+    "revision": "f48cfe0d6d7322dd6a6d22bf90541493",
+    "url": "/aleksvalushko/portfolio/static/media/background6.f48cfe0d.jpg"
+  },
+  {
+    "revision": "33edb2268b4d3c97412167f3adc5e953",
+    "url": "/aleksvalushko/portfolio/static/media/doubleArrowDown.33edb226.svg"
+  },
+  {
+    "revision": "372335f36dfcb614cb10b35e13245f29",
+    "url": "/aleksvalushko/portfolio/static/media/facebook3.372335f3.svg"
+  },
+  {
+    "revision": "cfd4e6f5e322eba14a5226fc451e8d75",
+    "url": "/aleksvalushko/portfolio/static/media/github2.cfd4e6f5.svg"
+  },
+  {
+    "revision": "6d86dcff75551244c6d5d2a1f6236848",
+    "url": "/aleksvalushko/portfolio/static/media/github3.6d86dcff.svg"
+  },
+  {
+    "revision": "16c2a71b4f31cae6d6146196da91fee1",
+    "url": "/aleksvalushko/portfolio/static/media/html52.16c2a71b.svg"
+  },
+  {
+    "revision": "be3c8bf6ffed8da9fe388c83d602dc55",
+    "url": "/aleksvalushko/portfolio/static/media/js6.be3c8bf6.svg"
+  },
+  {
+    "revision": "4300067280d0aa96501f47daea6434f5",
+    "url": "/aleksvalushko/portfolio/static/media/json2.43000672.svg"
+  },
+  {
+    "revision": "a045a284b32d58d0c80364cbb3fa022b",
+    "url": "/aleksvalushko/portfolio/static/media/linkedin3.a045a284.svg"
+  },
+  {
+    "revision": "739376b593d9f9c28468b6259c824131",
+    "url": "/aleksvalushko/portfolio/static/media/myPhoto.739376b5.jpg"
+  },
+  {
+    "revision": "1b2bce3d8fb75e9fb0499bef551b3a0c",
+    "url": "/aleksvalushko/portfolio/static/media/project2.1b2bce3d.png"
+  },
+  {
+    "revision": "64eca7c2099428962cd2804fd8195e4c",
+    "url": "/aleksvalushko/portfolio/static/media/react2.64eca7c2.svg"
+  },
+  {
+    "revision": "f30eeb17c22a3ce33eecc23bca4c4d74",
+    "url": "/aleksvalushko/portfolio/static/media/redux2.f30eeb17.svg"
+  },
+  {
+    "revision": "6a145072dfcca657a7abe0eb182a17e3",
+    "url": "/aleksvalushko/portfolio/static/media/sass2.6a145072.svg"
+  },
+  {
+    "revision": "0b2b5890c0b5f1231cb4b5268456c63b",
+    "url": "/aleksvalushko/portfolio/static/media/todolist.0b2b5890.png"
+  },
+  {
+    "revision": "f686c6c49225c89351f0c1838a4defa8",
+    "url": "/aleksvalushko/portfolio/static/media/twitter3.f686c6c4.svg"
+  },
+  {
+    "revision": "5459d993a51e563ba30a15f05316f92c",
+    "url": "/aleksvalushko/portfolio/static/media/up-arrow.5459d993.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9e3a0b3fe0c9179c0a23cf399980555.js"
+  "/aleksvalushko/portfolio/precache-manifest.f2023681e4b51557bd9e7fc0fd8c5f04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aleksvalushko/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
