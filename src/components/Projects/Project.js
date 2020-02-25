d4e777ab7e8a1d040c3bb773045da798ff14e82e
aleksJoker <aleks.valushko@yandex.ru>
2020-02-25 23:23:27 +0300

    added project link

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -5,10 +5,23 @@ function Project({project}) {
 
     let front = (project.id === 1) ? mod.front1 : mod.front2;
     let bottom = (project.id === 1) ? mod.bottom1 : mod.bottom2;
+debugger
+    let href;
+    switch (project.id){
+        case 1:
+            href =  'http://aleksvalushko.github.io/todolist';
+            break;
+        case 2:
+            href = '#';
+            break;
+        default:
+            href = '#';
+            break;
+    };
 
     return (
         <div>
-            <a href="#" className={mod.projectsItemImage}>
+            <a href={href} className={mod.projectsItemImage}>
                 <h2>{project.title}</h2>
                 <div className={mod.top}>
                     <div className={`${front} ${mod.face}`}></div>
